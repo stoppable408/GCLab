7ce73b1872329886ba29b8fb2169b844f0d97921
Lennon Turner <Lennonturner92@gmail.com>
2016-03-29 23:07:05 -0400

    Adds an updated version of Lab02 with more fuctions and a smaller main method

--- Lab02.java
+++ Lab02.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+  import java.util.Scanner;
 
 public class Lab02 {
 
@@ -15,11 +15,11 @@ public class Lab02 {
 
 	public static int health(double number) {
 		if (number < 3) {
-			return ((int) (Math.random() * 10) + 200);
+			return ((int) (Math.random() * 10) + 110);
 		} else if ((number > 3) && (number < 6)) {
-			return ((int) ((Math.random() * 50 + 400)));
+			return ((int) ((Math.random() * 50 + 220)));
 		} else {
-			return ((int) ((Math.random() * 20) + 100));
+			return ((int) ((Math.random() * 20) + 75));
 		}
 	}
 
@@ -35,6 +35,7 @@ public class Lab02 {
 			if (response.equalsIgnoreCase("flee")) {
 				int attempt = (int) (Math.random() * 100);
 				if (attempt > 50) {
+					System.out.println("Your attempt to flee has succeeded");
 					return true;
 				} else {
 					System.out.println("You fail at your attempt to flee, and the " + monsterName + " brings you back into battle and forces you to strike again!");
@@ -58,6 +59,120 @@ public class Lab02 {
 		}
 	}
 
+	
+	public static void bow(String monsterName, int health, Scanner input){
+		health = bowAttack(monsterName, health, input);
+		if (death(monsterName, health)) {
+			return;
+		}
+		if (flee(monsterName, health, input)) {
+			return;
+		} else {
+			health = bowAttack(monsterName, health, input);
+		}
+		if (death(monsterName, health)) {
+			return;
+		}
+		if (flee(monsterName, health, input)) {
+			return;
+		} else {
+			System.out.println("The " + monsterName
+					+ " musters all of it's strength, and swings at your head, instantly crushing your skull. You've died, and subsequently failed your people.");
+			return;
+		}
+	}
+	
+	public static void sword(String monsterName, int health, Scanner input){
+		health = swordAttack(monsterName, health, input);
+		if (death(monsterName, health)) {
+			return;
+		}
+		if (flee(monsterName, health, input)) {
+			return;
+		} 
+		else {
+		health = swordAttack(monsterName, health, input);	
+		}
+		if (death(monsterName, health)) {
+			return;
+		}
+		if (flee(monsterName, health, input)) {
+			return;
+		} else {
+			System.out.println("The " + monsterName
+					+ " musters all of it's strength, and swings at your head, instantly crushing your skull. You've died, and subsequently failed your people.");
+			return;
+		}
+	}
+	
+	public static int bowAttack(String monsterName, int health, Scanner input){
+		int accuracy = (int) ((Math.random()) * 10);
+		int damage = 0;
+		switch (accuracy){
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+			default:
+			damage = (int) (Math.random() * 10) + 40;
+		    health -= damage;
+			System.out.println("\nYou fire your bow at the " + monsterName + "'s chest, dealing " + damage + " damage to the beast. It has " + health + " health remaining");
+			return health;
+		case 5:
+		case 6:
+			damage = (int) (Math.random() * 20);
+			health -= damage;
+			System.out.println("\nYou fire your bow at the " + monsterName + "'s foot, dealing " + damage + " damage to the beast. it has " + health + " health remaining");
+			return health;
+		case 7:
+		case 8:
+		case 9:
+			damage = (int) (Math.random() * 10) + 60;
+			health -= damage;
+			System.out.println("\nYou fire your bow at the " + monsterName + "'s head, dealing " + damage + " damage to the beast. it has " + health + " health remaining");
+			return health;
+		case 10:
+			damage = (int) (Math.random() * 20) + 100;
+			health -= damage;
+			System.out.println("\nYou fire your bow at the " + monsterName + "'s eye, dealing a critical hit of " + damage + " damage to the beast. it has " + health + " health remaining");
+			return health;
+		}
+		
+	}
+	
+	public static int swordAttack(String monsterName, int health, Scanner input){
+		int accuracy = (int) ((Math.random()) * 10);
+		int damage = 0;
+		switch (accuracy){
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+			default:
+			damage = (int) (Math.random() * 10) + 60;
+		    health -= damage;
+			System.out.println("\nYou swing at the " + monsterName + "'s chest, dealing " + damage + " damage to the beast. It has " + health + " health remaining");
+			return health;
+		case 5:
+		case 6:
+			damage = (int) (Math.random() * 10);
+			health -= damage;
+			System.out.println("\nYou swing at the " + monsterName + "'s foot, dealing " + damage + " damage to the beast. it has " + health + " health remaining");
+			return health;
+		case 7:
+		case 8:
+		case 9:
+			damage = (int) (Math.random() * 10) + 85;
+			health -= damage;
+			System.out.println("\nYou swing at the " + monsterName + "'s head, dealing " + damage + " damage to the beast. it has " + health + " health remaining");
+			return health;
+		case 10:
+			damage = (int) (Math.random() * 20) + 200;
+			health -= damage;
+			System.out.println("\nYou swing at the " + monsterName + "'s eye, dealing a critical hit of " + damage + " damage to the beast. it has " + health + " health remaining");
+			return health;
+		}
+	}
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		boolean isSwordTrue, isBowTrue;
@@ -76,74 +191,11 @@ public class Lab02 {
 				String weapon = input.next();
 				isSwordTrue = weapon.equalsIgnoreCase("sword");
 				isBowTrue = weapon.equalsIgnoreCase("bow");
+				
 				if (weapon.equalsIgnoreCase("bow")) {
-
-					int bowDamage = (int) ((Math.random() * 7) + 40);
-					health -= bowDamage;
-					System.out.println("\nthe " + monsterName
-							+ " lunges at you, scratching you with his large claws. You fall down, grasp your bow and arrow,\nand launch an arrow at the "
-							+ monsterName + " hitting it for " + bowDamage + " damage, and leaving it with " + health
-							+ " health.");
-					if (death(monsterName, health)) {
-						break;
-					}
-					if (flee(monsterName, health, input)) {
-						System.out.println("Your attempt to flee has succeeded");
-						break;
-					} else {
-						int critDamage = bowDamage * 2;
-						health -= critDamage;
-						System.out.println("\nThe " + monsterName
-								+ " glares at you menacingly and swipes at you with a crippling blow. You know that you don't have much time left to defeat it. \nyou grasp your bow, slowly draw the bowstring back, and fire a bow straight into the "
-								+ monsterName + "'s eye! it's a critical hit. dealing " + critDamage
-								+ " damage, and leaving it with " + health + " health.");
-					}
-					if (death(monsterName, health)) {
-						break;
-					}
-					if (flee(monsterName, health, input)) {
-						System.out.println("Your attempt to flee has succeeded");
-						break;
-					} else {
-						System.out.println("The " + monsterName
-								+ " musters all of it's strength, and swings at your head, instantly crushing your skull. You've died, and subsequently failed your people.");
-						break;
-					}
-
+					bow(monsterName,health,input);
 				} else if (weapon.equalsIgnoreCase("sword")) {
-					int swordDamage = (int) (((Math.random() * 10) + 70));
-					health -= swordDamage;
-					System.out.println("\nYou quickly leap toward the sword, grasp it with you hand, and swing at the "
-							+ monsterName + " with maximum force! hitting it for " + swordDamage
-							+ " damage, and leaving it with " + health + " health");
-
-					if (death(monsterName, health)) {
-						break;
-					}
-
-					if (flee(monsterName, health, input)) {
-						System.out.println("Your attempt to flee has succeeded");
-						break;
-					} else {
-						int critDamage = swordDamage * 2;
-						health -= critDamage;
-						System.out.println("\nThe " + monsterName
-								+ " swings at you with it's claws, dealing a crippling blow. You stand up, muster your strength, and swing your sword at the "
-								+ monsterName + "'s chest. dealing a critical blow of " + critDamage
-								+ " damage. Leaving it with " + health + " health.");
-					}
-					if (death(monsterName, health)) {
-						break;
-					}
-					if (flee(monsterName, health, input)) {
-						System.out.println("Your attempt to flee has succeeded");
-						break;
-					} else {
-						System.out.println("The " + monsterName
-								+ " musters all of it's strength, and swings at your head, instantly crushing your skull. You've died, and subsequently failed your people.");
-						break;
-					}
-
+					sword(monsterName, health, input);
 				} else {
 					System.out.println("You did not type \"bow\" or \"sword\" correctly. Please try again: ");
 				}
