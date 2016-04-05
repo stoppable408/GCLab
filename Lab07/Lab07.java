4e42e537860bf4ad3766967dcb2eb77c7d5fa334
Lennon Turner <Lennonturner92@gmail.com>
2016-04-05 18:09:55 -0400

    Adds Lab07

--- Lab07/Lab07.java
+++ Lab07/Lab07.java
@@ -1,42 +1,97 @@
 import java.util.Scanner;
 
 public class Lab07 {
-
-	public static int getNum(Scanner input) {
+	static String firstName, lastName, timeOfRule, death;
+	static Scanner input = new Scanner(System.in);
+	static String answer;
+	public static int getNum() {
 
 		while (!input.hasNextInt()) {
 			System.out.print("Invalid input. Please enter a valid number: ");
 			input.nextLine();
 		}
 		int number = input.nextInt();
-		if ((number < 0) || (number > 20)) {
+		if ((number < 0) || (number > 11)) {
 			do {
-				System.out.println("That emperor does not exist. Please try again. (enter a number 1-20): ");
+				System.out.println("That emperor does not exist. Please try again. (enter a number 1-11): ");
 				number = input.nextInt();
-			} while ((number < 0) || (number > 20));
+			} while ((number < 0) || (number > 11));
 		}
 
 		return number;
 
 	}
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		String firstName, lastName, timeOfRule, death;
-		System.out.print(
-				"Welcome to the Classical Roman Emperor Database!. \nWhich emperor would you like to learn more about?\n(Enter a number 1-20): ");
-		Emperor[] emperorList = Emperor.populateArray();
-		int emperorNumber = getNum(input);
+	public static void timeOrDeath() {
+		String answer;
+		boolean isTime = false, isDeath = false;
 
-		firstName = emperorList[emperorNumber].getFirstName();
-		lastName = emperorList[emperorNumber].getLastName();
-		timeOfRule = emperorList[emperorNumber].getTimeInOffice();
-		death = emperorList[emperorNumber].getDeath();
-
-		System.out.print("Emperor Number " + emperorNumber + " is " + firstName + " " + lastName
-				+ ". \nWhat would you like to know about " + firstName
+		System.out.println("\nWhat would you like to know about " + firstName
 				+ "? \n(Enter \"time\" for Time in Office, or \"death\" for Date of Death.): ");
+		do {
 
+			answer = input.next();
+
+			if (answer.equalsIgnoreCase("time")) {
+				System.out.println(firstName + " " + lastName + " spent " + timeOfRule + " in Office.");
+				isTime = true;
+			} else if (answer.equalsIgnoreCase("death")) {
+				System.out.println(firstName + " " + lastName + " died on " + death);
+				isDeath = true;
+			} else {
+				System.out.println("You entered an incorrect word. Please try again: ");
+			}
+		} while (!(isTime || isDeath));
+	}
+
+	public static void main(String[] args) {
+		boolean isKnow = false, isChoose = false;
+		Emperor[] emperorList = Emperor.populateArray();
+
+		do {
+			
+			System.out.print(
+					"Welcome to the Classical Roman Emperor Database!. \nWhich emperor would you like to learn more about?\n(Enter a number 1-11): ");
+
+			int emperorNumber = getNum();
+
+			firstName = emperorList[emperorNumber - 1].getFirstName();
+			lastName = emperorList[emperorNumber - 1].getLastName();
+			timeOfRule = emperorList[emperorNumber - 1].getTimeInOffice();
+			death = emperorList[emperorNumber - 1].getDeath();
+
+			System.out.print("Emperor Number " + emperorNumber + " is " + firstName + " " + lastName + ".");
+
+			timeOrDeath();
+
+			
+			do {
+				isChoose = false;
+				System.out.println(
+						"\nWould you like to know more?\nWould you like to choose another Emperor?\nor Would you like to quit?: ");
+				System.out.println("\nType \"know\" to get more information about you current Emperor.");
+				System.out.println("Type \"choose\" to choose another Emperor.");
+				System.out.print("otherwise, type anything to quit: ");
+				answer = input.next();
+				if (answer.equalsIgnoreCase("know")) {
+					timeOrDeath();
+				} 
+				else if (answer.equalsIgnoreCase("choose")) {
+					isChoose = true;
+					continue;
+				}
+				else if (answer.equalsIgnoreCase("quit")){
+					break;
+				}
+				else{
+					System.out.println("Invalid Entry.");
+					isChoose = false;
+				}
+				
+			} while (!isChoose);
+		} while ((isKnow)||(isChoose));
+		
+System.out.println("\nThank you for accessing the Emperor Database!");
 	}
 
 }
