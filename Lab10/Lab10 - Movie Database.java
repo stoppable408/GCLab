a430349426bc4adbeea31acf8e975c1f2591bb62
Lennon Turner <Lennonturner92@gmail.com>
2016-04-06 08:44:26 -0400

    Adds a musical and comedy category for the Movie list

--- Lab10/Lab10 - Movie Database.java
+++ Lab10/Lab10 - Movie Database.java
@@ -8,16 +8,18 @@ public class Lab10 {
 	public static void main(String[] args) {
 		boolean answered = true;
 		Scanner input = new Scanner(System.in);
-		int scifiCounter = 0, animatedCounter = 0, horrorCounter = 0, dramaCounter = 0;
+		int scifiCounter = 0, animatedCounter = 0, horrorCounter = 0, dramaCounter = 0, musicalCounter = 0, comedyCounter = 0;
 		String answer;
-		boolean isScifi, isAnimated, isDrama, isHorror;
+		boolean isScifi, isAnimated, isDrama, isHorror, isMusical, isComedy;
 		System.out.println("Welcome to the Movie List Application!");
 		ArrayList<Movie> array = new ArrayList<Movie>(MAX);
 		ArrayList<Movie> scifi = new ArrayList<Movie>();
 		ArrayList<Movie> animated = new ArrayList<Movie>();
 		ArrayList<Movie> drama = new ArrayList<Movie>();
 		ArrayList<Movie> horror = new ArrayList<Movie>();
-
+		ArrayList<Movie> musical = new ArrayList<Movie>();
+		ArrayList<Movie> comedy = new ArrayList<Movie>();
+		
 		for (int i = 1; i < MAX; i++) {
 			array.add(i - 1, Movie.getMovie(i));
 
@@ -25,6 +27,8 @@ public class Lab10 {
 			isAnimated = array.get(i - 1).getCategory().equalsIgnoreCase("animated");
 			isDrama = array.get(i - 1).getCategory().equalsIgnoreCase("drama");
 			isHorror = array.get(i - 1).getCategory().equalsIgnoreCase("horror");
+			isMusical = array.get(i - 1).getCategory().equalsIgnoreCase("musical");
+			isComedy = array.get(i - 1).getCategory().equalsIgnoreCase("comedy");
 
 			if (isScifi) {
 				scifi.add(scifiCounter, Movie.getMovie(i));
@@ -40,13 +44,21 @@ public class Lab10 {
 				horror.add(horrorCounter, Movie.getMovie(i));
 				horrorCounter++;
 			}
+			else if(isMusical){
+				musical.add(musicalCounter, Movie.getMovie(i));
+				musicalCounter++;
+			}
+			else if(isComedy){
+				comedy.add(comedyCounter, Movie.getMovie(i));
+				comedyCounter++;
+			}
 
 		}
 
 		System.out.println("\nThere are 100 movies in this list.");
 		do {
 			System.out.println("What category are you interested in?");
-			System.out.print("\t\t  '1' Animated\n\t\t  '2' Drama\n\t\t  '3' Horror\n\t\t  '4' Scifi");
+			System.out.print("\t\t  '1' Animated\n\t\t  '2' Drama\n\t\t  '3' Horror\n\t\t  '4' Scifi\n\t\t  '5' Musical\n\t\t  '6'Comedy");
 
 			System.out.print("\nInput Answer Here: ");
 			do {
@@ -72,7 +84,22 @@ public class Lab10 {
 					for (int i = 0; i < scifi.size(); i++) {
 						System.out.println(scifi.get(i).getTitle());
 					}
-				} else {
+					
+				} 
+				else if (answer.equalsIgnoreCase("5")){
+					Collections.sort(musical, new Movie());
+					for (int i = 0; i < musical.size(); i++) {
+						System.out.println(musical.get(i).getTitle());
+					}
+					
+				}
+				else if(answer.equalsIgnoreCase("6")){
+					Collections.sort(comedy, new Movie());
+					for (int i = 0; i < comedy.size(); i++) {
+						System.out.println(comedy.get(i).getTitle());
+					}
+				}
+				else {
 					answered = false;
 					System.out.print("Invalid Input. Input another number: ");
 				}
